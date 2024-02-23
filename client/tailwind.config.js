6014fd89c6dbfc092341011d49b1bb23612bb9c5
Rocobros <rocobros21@gmail.com>
2024-02-22 22:25:38 -0600

    Added tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '0 5px 15px rgba(0, 0, 0, 0.5)',
+      }
+    },
+    colors: {
+    'text': '#081603',
+    'background': '#f4fdf2',
+    'primary': '#59a73c',
+    'secondary': '#8aefc0',
+    'accent': '#54e8d2',
+    'transparent': 'transparent',
+    },
+    fontFamily: {
+      primary: ['Roboto', 'sans-serif'],
+      secondary: ['Nunito', 'serif']
+    },
   },
   plugins: [],
 }
