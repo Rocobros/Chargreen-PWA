eaa39b223749562dc6aff9213e38016e7a3ce3a8
Rocobros <rocobros21@gmail.com>
2024-03-08 13:59:04 -0600

    Changed queries for new DB

--- server/server.js
+++ server/server.js
@@ -1,22 +1,16 @@
 const express = require('express')
 const mysql = require('mysql')
-const cors = require ('cors')
+const cors = require('cors')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-try {
-    
-} catch (error) {
-    
-}
-
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'pwa_db'
+    database: 'chargreen_db'
 })
 
 app.get('/fk', (req, res) => {
@@ -30,7 +24,7 @@ app.get('/fk', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    const sql = "SELECT * FROM credentials WHERE `username` = ? AND `password` = ?"
+    const sql = "SELECT * FROM credentials WHERE `usuario` = ? AND `contrasena` = ?"
     db.query(sql, [req.body.username, req.body.password], (err, data) => {
         if(err){
             return res.json('Error querying from credentials table')
@@ -45,7 +39,7 @@ app.post('/login', (req, res) => {
 })
 
 app.post('/register/credentials', (req, res) => {
-    const credentials = 'INSERT INTO credentials (`username`, `password`) VALUES (?)'
+    const credentials = 'INSERT INTO credentials (`usuario`, `contrasena`) VALUES (?)'
 
     const values = [req.body.user[0], req.body.pass[0]]
     
@@ -60,8 +54,8 @@ app.post('/register/credentials', (req, res) => {
 
 app.post('/register/user', (req, res) => {
 
-    const sql = 'INSERT INTO `users`(`name`, `apep`, `apem`, `cel`, `email`, `credentials_id`) VALUES (?)'
-    const vals = [req.body.name[0], req.body.apep[0], req.body.apem[0], req.body.tel[0], req.body.email[0], req.body.fk[0]]
+    const sql = 'INSERT INTO `usuariosnormales`(`nombre`, `apellidopaterno`, `apellidomaterno`, `celular`, `correo`, `nivel`, `fk_credentials`) VALUES (?)'
+    const vals = [req.body.name[0], req.body.apep[0], req.body.apem[0], req.body.tel[0], req.body.email[0], 1, req.body.fk[0]]
 
     db.query(sql, [vals], (err, data) => {
         if(err){
