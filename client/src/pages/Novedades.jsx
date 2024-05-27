7fb495e633055147e1b4419e214b4484d368682b
Rocobros <rocobros21@gmail.com>
2024-05-27 08:33:35 -0600

    Finished Novedades

--- client/src/pages/Novedades.jsx
+++ client/src/pages/Novedades.jsx
@@ -4,10 +4,16 @@ import Tarjeta from '../components/Novedades/Tarjeta'
 
 const Novedades = () => {
   const [novedades, setNovedades] = useState([])
+  const [filteredNovedades, setFilteredNovedades] = useState([])
   const [expandedCard, setExpandedCard] = useState(null)
+  const [filter, setFilter] = useState('N') // Default filter to 'Novedades'
 
   const handleCardClick = (index) => {
     setExpandedCard(expandedCard === index ? null : index)
+  }
+
+  const handleFilterChange = (type) => {
+    setFilter(type)
   }
 
   useEffect(() => {
@@ -19,6 +25,10 @@ const Novedades = () => {
     fetchNovedades()
   }, [])
 
+  useEffect(() => {
+    setFilteredNovedades(novedades.filter((novedad) => novedad.Tipo === filter))
+  }, [novedades, filter])
+
   return (
     <>
       <header className="flex items-center p-4 border-b border-gray-300 bg-primary">
@@ -26,15 +36,34 @@ const Novedades = () => {
           Novedades y actualizaciones
         </h1>
       </header>
+      <div className="text-center flex my-2 gap-2">
+        <button
+          className={`flex-1 bg-primary text-text border-none py-2.5 px-5 m-1 cursor-pointer text-base ml-4 ${
+            filter === 'N' ? 'bg-secondary' : ''
+          }`}
+          onClick={() => handleFilterChange('N')}
+        >
+          Novedades
+        </button>
+        <button
+          className={`flex-1 bg-primary text-text border-none py-2.5 px-5 m-1 cursor-pointer text-base mr-4 ${
+            filter === 'A' ? 'bg-secondary' : ''
+          }`}
+          onClick={() => handleFilterChange('A')}
+        >
+          Actualizaciones
+        </button>
+      </div>
       <div className="flex flex-col items-center w-screen h-screen overflow-hidden p-4 space-y-4">
-        <div className="w-full max-w-md">
-          {novedades.map((novedad, index) => (
+        <div className="w-full max-w-md h-full overflow-y-auto">
+          {filteredNovedades.map((novedad, index) => (
             <Tarjeta
               key={novedad.Id}
               title={novedad.Titulo}
-              date={novedad.Fecha}
+              date={new Date(novedad.Fecha).toDateString()}
               image={novedad.Imagen}
               description={novedad.Descripcion}
+              link={novedad.Link}
               isExpanded={expandedCard === index}
               onClick={() => handleCardClick(index)}
             />
