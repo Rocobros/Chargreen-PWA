7d8e96894915f06a271bd4ca571c4bac45e760c0
Rocobros <rocobros21@gmail.com>
2024-05-30 13:18:27 -0600

    Put code in registro table

--- server/utils/crearRegistro.js
+++ server/utils/crearRegistro.js
@@ -6,14 +6,21 @@ async function crearRegistro(data) {
       'SELECT Id FROM salidas WHERE TorreCarga = ? AND Numero = ?',
       [data.id, data.salida]
     )
-    await pool.execute(
-      'INSERT INTO `registro`(`Botella`, `Salida`) VALUES (?, ?)',
+    const [result] = await pool.execute(
+      'INSERT INTO `registro`(`Botella`, `Salida`, `Codigo`) VALUES (?, ?, ?)',
       [data.botella, results[0].Id]
     )
-		await pool.execute(
-      "UPDATE salidas SET Estado = 'A' WHERE Id = ?",
+    const [code] = await pool.execute(
+      'SELECT Codigo FROM salidas WHERE Id = ?',
       [results[0].Id]
     )
+    await pool.execute("UPDATE salidas SET Estado = 'A' WHERE Id = ?", [
+      results[0].Id,
+    ])
+    await pool.execute('UPDATE registro SET Codigo = ? WHERE Id = ?', [
+      code[0].Codigo,
+      result.insertId,
+    ])
   } catch (error) {
     console.error(error.message)
   }
